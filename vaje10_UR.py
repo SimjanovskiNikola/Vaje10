b0c88d195f14ae2a2b646b315e416b33ff14c397
SimjanovskiNikola <76400220+SimjanovskiNikola@users.noreply.github.com>
2020-12-20 17:40:05 +0100

    Update vaje10_UR.py

--- vaje10_UR.py
+++ vaje10_UR.py
@@ -1,1 +1,14 @@
-Hello 
+import math
+# num = int(input("Please, enter a number."))
+
+for j in range (2, 100):
+    until = int(math.sqrt(j))
+    res = False
+
+    for i in range(2, until+1):
+        if j % i == 0:
+            res = True
+            break
+
+    if res == False:
+        print(j)
